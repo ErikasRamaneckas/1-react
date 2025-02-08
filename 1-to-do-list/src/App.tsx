98139ff967da261ee4ca9d636b78e1910013d7c4
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-02-08 12:20:35 +0200

    Create base components

--- 1-to-do-list/src/App.tsx
+++ 1-to-do-list/src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css';
+import TaskForm from './components/TaskForm.tsx';
+import TaskList from './components/TaskList.tsx';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <TaskForm />
+      <TaskList />
+    </>
+  );
 }
 
 export default App;
