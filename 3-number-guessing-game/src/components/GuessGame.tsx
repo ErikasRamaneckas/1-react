fc6758b7e0ee74a3b33c2430e89801e804c92a0a
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-02-11 12:08:13 +0200

    Add random number generation from 1 to 100

--- 3-number-guessing-game/src/components/GuessGame.tsx
+++ 3-number-guessing-game/src/components/GuessGame.tsx
@@ -1,9 +1,15 @@
 import GuessForm from './GuessForm.tsx';
 import Message from './Message.tsx';
 
+function generateRandNum() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
 export default function GuessGame() {
+  generateRandNum();
   return (
     <>
+      <h3>{generateRandNum()}</h3>
       <GuessForm />
       <Message />
     </>
