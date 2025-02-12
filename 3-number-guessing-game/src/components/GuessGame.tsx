6fa0a41fb5634a7f120d9e632214e29b3d577f85
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-02-12 16:13:12 +0200

    Add number guessing game messages

--- 3-number-guessing-game/src/components/GuessGame.tsx
+++ 3-number-guessing-game/src/components/GuessGame.tsx
@@ -10,14 +10,22 @@ export default function GuessGame() {
   const [randNum] = useState(generateRandNum());
   const [guess, setGuess] = useState('');
   const [numberOfGuesses, setNumberOfGuesses] = useState(0);
+  const [message, setMessage] = useState('');
 
   function handleGuess(e) {
     e.preventDefault();
     console.log('Form submitted!');
     if (Number(guess) === randNum) {
       console.log('you win!');
+      setMessage('You win');
+      return;
     }
     setNumberOfGuesses(numberOfGuesses + 1);
+    if (Number(guess) < randNum) {
+      setMessage('too low!');
+    } else {
+      setMessage('too high!');
+    }
   }
   return (
     <>
@@ -28,6 +36,7 @@ export default function GuessGame() {
         setGuess={setGuess}
         onGuess={handleGuess}
       />
+      <Message message={message} />
     </>
   );
 }
