886d8c6f38fee771cc8c561d5de1327b7325d6de
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-02-13 16:36:00 +0200

    Improve todo form

--- 1-to-do-list/src/components/TaskForm.tsx
+++ 1-to-do-list/src/components/TaskForm.tsx
@@ -2,21 +2,24 @@ import { useState } from 'react';
 
 export default function TaskForm({ onAddTodo }) {
   const [title, setTitle] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onAddTodo(title);
+    setTitle('');
+  }
+
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <input
         placeholder="Add todo"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
+        minLength={3}
+        maxLength={256}
+        required
       />
-      <button
-        onClick={() => {
-          setTitle('');
-          onAddTodo(title);
-        }}
-      >
-        Add
-      </button>
-    </>
+      <button type="submit">Add</button>
+    </form>
   );
 }
